734430a6041061b3adb24ce5a1db00c9f34ae396
Sami Rosenblad <rosenblad@gmail.com>
2016-10-12 10:05:13 +0300

    Whitelisted local addresses for console access

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Whitelist our virtual IP address block to the web console.
+  config.web_console.whitelisted_ips = '10.2.0.0/16'
 end
