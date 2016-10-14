ac694830bf20eb6fdacda788b6ce2cee93e4ffdd
Sami Rosenblad <rosenblad@gmail.com>
2016-10-14 17:25:26 +0300

    Added a scope and method for active users, made Devise ensure users are active when authenticating

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :lockable, :recoverable,
          :rememberable, :trackable, :timeoutable, :validatable
 
+  scope :active, -> { where %q{
+    (users.activates_at IS NULL OR users.activates_at >= :today) AND
+    (users.expires_at IS NULL OR users.expires_at > :today)
+  }, today: Date.current}
+
   validates :last_name, :first_names, presence: true
+
+  def active?
+    (activates_at.nil? || !activates_at.future?) &&
+    (expires_at.nil? || expires_at.future?)
+  end
+
+  # Overrides Devise method to ensure user is active.
+  def active_for_authentication?
+    super && active?
+  end
 
   def full_name
     return last_name if first_names.empty?
