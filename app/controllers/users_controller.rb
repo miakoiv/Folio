9330632d2bb799149f47652eeae72b8799a73507
Sami Rosenblad <rosenblad@gmail.com>
2016-10-20 15:49:17 +0300

    Added a default locale for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    @user = User.new
+    @user = User.new(locale: I18n.default_locale)
   end
 
   # GET /users/1/edit
