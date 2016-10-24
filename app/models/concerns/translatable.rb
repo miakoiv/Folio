e835e09d401ab48c75b36e9e5e435907672adf24
Sami Rosenblad <rosenblad@gmail.com>
2016-10-24 14:13:56 +0300

    Added a locale parameter to Translatable#name

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -4,7 +4,7 @@
 #
 # Including Translatable adds a localized_name_method class method
 # that returns the method name corresponding to the given locale,
-# defaulting to given default if no matching attribute can be found.
+# defaulting to I18n.default_locale if no matching attribute can be found.
 # This method is used to define the default scope and a name method.
 #
 module Translatable
@@ -15,13 +15,13 @@ module Translatable
   end
 
   class_methods do
-    def localized_name_method(locale = I18n.locale, default = :fi)
+    def localized_name_method(locale = I18n.locale)
       method = "name_#{locale}"
-      has_attribute?(method) && method || "name_#{default}"
+      has_attribute?(method) && method || "name_#{I18n.default_locale}"
     end
   end
 
-  def name
-    send(self.class.localized_name_method)
+  def name(locale = I18n.locale)
+    send(self.class.localized_name_method(locale))
   end
 end
