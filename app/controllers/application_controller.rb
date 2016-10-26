ee684a24442478708a1df71f14f4794f858a029e
Sami Rosenblad <rosenblad@gmail.com>
2016-10-26 11:55:48 +0300

    Made ApplicationController set and return the current unit, added current unit name to the profile picture partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
   before_action :authenticate_user!
+  before_action :set_unit
   after_action :prepare_unobtrusive_flash
 
   # Set a flag to disable editing in rendered forms.
@@ -9,10 +10,22 @@ class ApplicationController < ActionController::Base
     @disable_editing = true
   end
 
+  # The unit the current user is working at, see #set_unit.
+  def current_unit
+    @current_unit
+  end
+  helper_method :current_unit
+
   private
     def set_locale
       I18n.locale = params[:locale] ||
         user_signed_in? && current_user.locale.presence ||
         I18n.default_locale
+    end
+
+    # As users are not associated with units yet,
+    # the first unit is selected by default.
+    def set_unit
+      @current_unit = Unit.first
     end
 end
