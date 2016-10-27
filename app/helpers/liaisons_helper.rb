9b3d6e754c924c8b958f545f43ad5427d3608598
Sami Rosenblad <rosenblad@gmail.com>
2016-10-27 11:09:15 +0300

    Added form controls to select liaison referrer and optionally specify referrer info

--- app/helpers/liaisons_helper.rb
+++ app/helpers/liaisons_helper.rb
@@ -3,4 +3,8 @@ module LiaisonsHelper
   def status_options
     Status.all.map { |s| [s.to_s, s.id, data: {label: s.appearance}] }
   end
+
+  def referrer_options
+    Referrer.all.map { |r| [r.to_s, r.id] }
+  end
 end
