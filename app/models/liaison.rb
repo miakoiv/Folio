49edd8918fd7319d9733e76ed744878679c251cf
Sami Rosenblad <rosenblad@gmail.com>
2016-10-31 14:48:05 +0200

    Added a creator association to liaisons, updated the scopes to reflect the new association structure

--- app/models/liaison.rb
+++ app/models/liaison.rb
@@ -1,19 +1,27 @@
 class Liaison < ApplicationRecord
 
+  belongs_to :creator, class_name: 'User'
   belongs_to :user
   belongs_to :person
+  has_many :events
 
   belongs_to :status
   belongs_to :referrer, optional: true
-  has_many :events
 
   default_scope { order(created_at: :desc) }
 
-  # Scope for restricting liaisons to a single unit.
-  scope :at, -> (unit) { where(unit: unit) }
+  # Scope for liaisons handled by user.
+  scope :for, -> (user) { where(user: user) }
+
+  # Scope for finding liaisons for all users in a unit.
+  scope :at, -> (unit) { joins(user: :unit).where(users: {unit_id: unit}) }
 
 
   def to_s
-    "#{created_at.to_date} / #{person}"
+    %Q{#{model_name.human.capitalize}
+      #{Liaison.human_attribute_name(:created_at).downcase}
+      #{created_at.to_date}
+      [#{status}]
+    }
   end
 end
