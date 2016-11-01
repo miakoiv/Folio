20d8034803b74db87833f76853bbc01ccecfdc5b
Sami Rosenblad <rosenblad@gmail.com>
2016-11-01 14:32:56 +0200

    Added event appearances by event type, method to check if event is topical, i.e. belongs to a given liaison

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,17 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   belongs_to :liaison
   belongs_to :event_type
+  delegate :appearance, to: :event_type
 
   default_scope { order(starts_at: :desc) }
 
   validates :title, presence: true
+
+
+  # An event is considered topical if it belongs to the given liaison.
+  def topical?(l)
+    liaison == l
+  end
 
   def to_s
     title
