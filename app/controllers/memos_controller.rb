be704fca83626f4f65119389f35cab25173b7319
Sami Rosenblad <rosenblad@gmail.com>
2016-11-03 17:19:09 +0200

    Added controller and views for memos

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,92 @@
 class MemosController < ApplicationController
+
+  before_action :set_memo, only: [:show, :edit, :update, :destroy]
+
+  # GET /memos
+  # GET /memos.json
+  def index
+    @memo_search_params = search_params
+    @search = MemoSearch.new(@memo_search_params)
+    @memos = @search.results.page(params[:page])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  # GET /memos/1
+  # GET /memos/1.json
+  def show
+    disable_editing!
+  end
+
+  # GET /memos/new
+  def new
+    @memo = current_user.sent_memos.build
+  end
+
+  # GET /memos/1/edit
+  def edit
+  end
+
+  # POST /memos
+  # POST /memos.json
+  def create
+    @memo = current_user.sent_memos.build(memo_params)
+
+    respond_to do |format|
+      if @memo.save
+        format.html { redirect_to memos_path, notice: t('.notice', memo: @memo) }
+        format.json { render :show, status: :created, location: @memo }
+      else
+        format.html { render :new }
+        format.json { render json: @memo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /memos/1
+  # PATCH/PUT /memos/1.json
+  def update
+    respond_to do |format|
+      if @memo.update(memo_params)
+        format.html { redirect_to @memo, notice: t('.notice', memo: @memo) }
+        format.json { render :show, status: :ok, location: @memo }
+      else
+        format.html { render :edit }
+        format.json { render json: @memo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /memos/1
+  # DELETE /memos/1.json
+  def destroy
+    @memo.destroy
+    respond_to do |format|
+      format.html { redirect_to memos_url, notice: t('.notice', memo: @memo) }
+      format.json { head :no_content }
+    end
+  end
+
+  protected
+    def search_params
+      params.fetch(:memo_search) {{}}.permit(:keyword).merge(
+        recipients: current_user
+      )
+    end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_memo
+      @memo = Memo.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def memo_params
+      params.require(:memo).permit(
+        :icon, :title, :content, recipient_ids: []
+      )
+    end
 end
--- app/helpers/memos_helper.rb
+++ app/helpers/memos_helper.rb
@@ -1,2 +1,11 @@
 module MemosHelper
+
+  def icon_options
+    %w{
+      smile-o meh-o frown-o
+      thumbs-o-up thumbs-o-down hand-peace-o hand-spock-o
+      comment-o warning
+      support star heart-o trophy
+    }
+  end
 end
