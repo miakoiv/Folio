4eef5415e5d63444b08965f2688ae987a1196999
Sami Rosenblad <rosenblad@gmail.com>
2016-11-04 18:24:53 +0200

    Added push button selector for event duration, updated language, added date/time labels

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,6 +4,14 @@ module EventsHelper
     EventType.all.map { |t| [t.to_s, t.id, data: {label: t.appearance}] }
   end
 
+  def duration_options
+    [].tap do |options|
+      (1800..14400).step(1800) do |d|
+        options << [duration_in_hours_minutes(d), d]
+      end
+    end
+  end
+
   def duration_in_hours_minutes(secs)
     mins = secs / 60
     return "#{mins}\u2008min" if mins < 60
