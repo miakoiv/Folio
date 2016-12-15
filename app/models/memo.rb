12d8c1dde055c881d1d11308e98ebaace6cf3b0c
Sami Rosenblad <rosenblad@gmail.com>
2016-12-15 16:15:18 +0200

    Added methods to get/set memo deliveries as a list of recipient global ids

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,11 +1,25 @@
 class Memo < ApplicationRecord
 
   belongs_to :sender, class_name: 'User'
-  has_many :deliveries
+  has_many :deliveries, dependent: :destroy
 
   default_scope { order(created_at: :desc) }
 
   validates :title, presence: true
+  validates :recipient_ids, presence: true
+
+
+  # Delivery recipients as a list of global ids.
+  def recipient_ids
+    deliveries.map { |d| d.recipient.to_global_id }
+  end
+
+  # Builds deliveries through a list of recipient global ids.
+  def recipient_ids=(list)
+    list.reject(&:blank?).map do |gid|
+      deliveries.build(recipient: GlobalID::Locator.locate(gid))
+    end
+  end
 
   def to_s
     title
