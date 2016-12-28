2aedb23fcfcb3293ddc2a60ccd1e7a771872635b
Sami Rosenblad <rosenblad@gmail.com>
2016-12-28 10:42:35 +0200

    Removed the activity tracker concern and moved its functionality into application controller since there was no advantage of having the single track method packaged as a concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,23 @@ class ApplicationController < ActionController::Base
   end
   helper_method :accessible_units
 
+  # Tracks activity for given resource in the scope of current unit and
+  # current user. Stores the action taken on given resource, storing any
+  # changes made as differences, except changes between two blank values.
+  def track(resource)
+    differences = resource.previous_changes
+      .except('created_at', 'updated_at')
+      .reject { |attribute, value| value.reject(&:blank?).empty? }
+
+    Activity.create(
+      unit: current_unit,
+      user: current_user,
+      action: action_name,
+      resource: resource,
+      differences: differences
+    )
+  end
+
   private
     def set_locale
       I18n.locale = params[:locale] ||
