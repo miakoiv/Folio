242d43ff48ba9128889c474a856e4d5ad86279f5
Sami Rosenblad <rosenblad@gmail.com>
2016-12-28 14:24:06 +0200

    Added icon and appearance methods to activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,4 +8,24 @@ class Activity < ApplicationRecord
 
   default_scope { order(id: :desc) }
 
+
+  ACTIONS = {
+       show: {icon: 'info-circle', appearance: 'info'},
+       edit: {icon: 'pencil-square', appearance: 'info'},
+     create: {icon: 'plus-circle', appearance: 'success'},
+     update: {icon: 'pencil-square', appearance: 'success'},
+    destroy: {icon: 'minus-circle', appearance: 'warning'}
+  }.freeze
+
+  def icon
+    Activity::ACTIONS[action.to_sym][:icon]
+  end
+
+  def appearance
+    Activity::ACTIONS[action.to_sym][:appearance]
+  end
+
+  def to_s
+    action
+  end
 end
