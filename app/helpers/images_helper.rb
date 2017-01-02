99efc54116bdc0a86b68e4b8de0784cade54f00d
Sami Rosenblad <rosenblad@gmail.com>
2017-01-02 14:38:35 +0200

    Added a required size argument to profile_image helper, added :thumbnail size to existing method calls

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,10 +1,10 @@
 module ImagesHelper
 
-  def profile_image(imageable, options = {})
+  def profile_image(imageable, size, options = {})
     options[:class] ||= ''
     options[:class] += ' img-circle'
     if imageable.respond_to?(:images) && imageable.images.any?
-      image_tag imageable.images.first.attachment_url(:thumbnail), options
+      image_tag imageable.images.first.attachment_url(size), options
     end
   end
 end
