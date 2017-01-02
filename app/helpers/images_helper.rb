e55767e0dc0ff2027b3d03272d1153a83d761b25
Sami Rosenblad <rosenblad@gmail.com>
2017-01-02 15:35:57 +0200

    Rolled back the addition of size argument to profile_image, added a data-printable attribute to hopefully have higher resolution printed images

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,10 +1,12 @@
 module ImagesHelper
 
-  def profile_image(imageable, size, options = {})
+  def profile_image(imageable, options = {})
     options[:class] ||= ''
     options[:class] += ' img-circle'
     if imageable.respond_to?(:images) && imageable.images.any?
-      image_tag imageable.images.first.attachment_url(size), options
+      first = imageable.images.first
+      image_tag first.attachment_url(:thumbnail),
+        options.merge('data-printable': first.attachment_url(:preview))
     end
   end
 end
