05262aa397dcc47065271ae447679da443ba4e90
Sami Rosenblad <rosenblad@gmail.com>
2017-01-02 14:35:21 +0200

    Added .img-circle to profile_image default options, removed explicit .img-circle options from method calls

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,7 +1,8 @@
 module ImagesHelper
 
   def profile_image(imageable, options = {})
-    options.reverse_merge!(class: 'img-circle')
+    options[:class] ||= ''
+    options[:class] += ' img-circle'
     if imageable.respond_to?(:images) && imageable.images.any?
       image_tag imageable.images.first.attachment_url(:thumbnail), options
     end
