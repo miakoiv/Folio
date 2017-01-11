a0177af519885f72ab64d2ddf516d2adee5bb6f3
Sami Rosenblad <rosenblad@gmail.com>
2017-01-11 16:40:00 +0200

    Added an option to specify where to store shrine.rb uploads via the SHRINE_STORAGE environment variable

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,8 +2,10 @@ require 'shrine'
 require 'shrine/storage/file_system'
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store'), # permanent
+  cache: Shrine::Storage::FileSystem.new('public',
+    prefix: ['uploads', ENV['SHRINE_STORAGE'], 'cache'].join('/')), # temporary
+  store: Shrine::Storage::FileSystem.new('public',
+    prefix: ['uploads', ENV['SHRINE_STORAGE'], 'store'].join('/')), # permanent
 }
 
 Shrine.plugin :activerecord
