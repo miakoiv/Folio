7e63642c6ab17ce321b34a44b906eb9994d64e2f
Sami Rosenblad <rosenblad@gmail.com>
2017-01-17 13:52:59 +0200

    Added Customer class methods to count customers by age and gender based on the associated Person

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -24,6 +24,16 @@ class Customer < ApplicationRecord
   # Scope for customers with notes by a user.
   scope :noted_by, -> (user) { joins(:notes).merge(Note.by(user)) }
 
+  def self.count_by_age
+    joins(:person).unscope(:order)
+      .where.not(people: {date_of_birth: nil})
+      .group(Person.age_function.to_sql).count
+  end
+
+  def self.count_by_gender
+    joins(:person).unscope(:order).group(:gender).count
+  end
+
   def date
     created_at.to_date
   end
