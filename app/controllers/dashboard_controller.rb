a3f16c2aba1cd4c4c70aa7dacfa4564d15aeadf9
Sami Rosenblad <rosenblad@gmail.com>
2017-01-30 14:18:30 +0200

    Added DashboardController#index to fetch the data displayed on the dashboard, added a separate tab for 'unit customers', or customers without a contact

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,9 @@
 class DashboardController < ApplicationController
 
+  def index
+    @user_customers = current_user.customers.at(current_unit)
+      .includes(:person).reorder(updated_at: :desc)
+    @unit_customers = current_unit.customers.without_contact
+    @top_notes = current_unit.notes.includes(customer: :person).limit(10)
+  end
 end
