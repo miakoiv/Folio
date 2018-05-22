c6feeb0ba3a12ae9dbdb29f9f847109c63ba0c8e
Sami Rosenblad <rosenblad@gmail.com>
2018-05-22 09:51:17 +0300

    Added helpers for resource related information for activities, dealing gracefully with the situation that the resource has since been deleted

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,19 @@
 module ActivitiesHelper
+
+  def resource_ident(activity)
+    "#{resource_class(activity).model_name.human} ##{activity.resource_id}"
+  end
+
+  def resource_class(activity)
+    if activity.resource.nil?
+      activity.resource_type.constantize
+    else
+      activity.resource.class
+    end
+  end
+
+  def resource_name(activity)
+    activity.resource ||
+      "#{t('.deleted')} #{resource_class(activity).model_name.human}"
+  end
 end
