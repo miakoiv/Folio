d0062c12c39e2087f9b5228c02980e1caf0a7038
Sami Rosenblad <rosenblad@gmail.com>
2018-05-22 16:04:57 +0300

    Made comments associate with deleted authors, changed the deleted record indicator to a new CSS class .deleted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   include Trackable
 
   belongs_to :post
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, -> { merge(User.including_deleted) }, class_name: 'User'
 
   default_scope { order(created_at: :asc) }
   scope :by, -> (author) { where(author: author) }
